698f6567e7c3f00b4fa9d8a11ec28fa6b4f61e53
scrunchhh <119765244+scrunchhh@users.noreply.github.com>
2022-12-15 01:30:44 -0500

    completed exercise 3 and 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, r, r2, r3 ,r4) {
+    let newList = []
+    for (let i = 0; i <= arr.length - 1; i++) {
+        if (arr[i] === r || arr[i] === r2 || arr[i] === r3 || arr[i] === r4 ) {
+            
+        } else {
+            newList.push(arr[i]) 
+        }
+    }
+    return newList
 };
 
 // Do not edit below this line
