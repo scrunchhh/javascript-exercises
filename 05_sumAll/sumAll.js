7264839b1c23d8d32a287851f37ea4f12e016d57
scrunchhh <119765244+scrunchhh@users.noreply.github.com>
2022-12-15 02:30:09 -0500

    Completed exercises 5, 6, and 7

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(start, stop) {
+    let num = 0
+    if (typeof(start) != 'number' || typeof(stop) != 'number' || start < 0 || stop < 0) {
+        return "ERROR"
+    } else if (stop < start) {
+        for (let i = stop; i <= start; i++) {
+            num += i 
+        }
+    } else {
+        for (let i = start; i <= stop; i++) {
+            num += i 
+        }
+    }
+    return num
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  temp = (temp - 32) * 0.5556 
+  return Math.round(temp * 10) / 10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  temp = (temp * 1.8) + 32
+  return Math.round(temp * 10) / 10
 };
 
 // Do not edit below this line
