070bcdf6d57dd13294ba58589e4b1581a3952d2d
scrunchhh <119765244+scrunchhh@users.noreply.github.com>
2022-12-14 23:56:49 -0500

    completed exercises 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(a, b) {
+    let o = ''
+    if (b < 0) {
+        o = "ERROR"
+        return o
+    }
+    else {
+        for (i = 1; i <= b; i++) {
+            o += a
+        }
+        return o 
+    }
+    
 };
 
 // Do not edit below this line
